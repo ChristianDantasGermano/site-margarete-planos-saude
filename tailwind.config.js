9bcf0f59efdf43c2b8dbe8b23804c472f3dbd46a
christiandantasgermano <christian.dantas.germano@gmail.com>
2023-10-24 15:02:51 +0000

    Modificado corroceu com flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}" , 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        'old_standard':['Old Standard TT', 'serif'],
-        'advent_pro': ['Advent Pro', 'sans-serif'],
-        'belleza': ['Belleza', 'sans-serif'],
-        'tenor_Sans': ['Tenor Sans', 'sans-serif'],
-        'antic_didone': ['Antic Didone', 'serif']
-       },
+        old_standard: ["Old Standard TT", "serif"],
+        advent_pro: ["Advent Pro", "sans-serif"],
+        belleza: ["Belleza", "sans-serif"],
+        tenor_Sans: ["Tenor Sans", "sans-serif"],
+        antic_didone: ["Antic Didone", "serif"],
+      },
       colors: {
         base: {
-          primary: '#EBE8F2',
-          secondary: '#D6D1E5',
-          tertiary: '#ADA2CA'
+          primary: "#EBE8F2",
+          secondary: "#D6D1E5",
+          tertiary: "#ADA2CA",
         },
-      }
+      },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 }
-
