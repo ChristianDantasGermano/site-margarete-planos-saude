9fa873df335ba9e765fcf187dd6735797850678b
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2023-11-16 14:35:51 +0000

    Adicionado uso de frame motion aos card de plano

--- src/components/PlanosDeSaude/components/CardPlanoSaude.tsx
+++ src/components/PlanosDeSaude/components/CardPlanoSaude.tsx
@@ -1,37 +1,50 @@
-import { useState } from "react"
+import { useState } from "react";
 import { planoLogos } from "..";
+import { motion } from "framer-motion";
 
 type CardPlanoSaude = {
-    index: number,
-    planos: typeof planoLogos[0]
-}
+  index: number;
+  planos: (typeof planoLogos)[0];
+};
 
 const CardPlanoSaude: React.FC<CardPlanoSaude> = ({ index, planos }) => {
-    const [showSaberMais, setShowSaberMais] = useState(false);
+  const animateSaberMaisHide = -30;
+  const animateSaberMaisShow = 0;
 
-    return (
-        <div className="flex flex-col">
-            <div className="flex flex-col justify-center items-center shadow-lg p-6 h-40
-             rounded-md transition ease-in-out delay-50 hover:scale-110 duration-100"
-                onMouseOver={() => setShowSaberMais(true)} onMouseOut={() => setShowSaberMais(false)}>
-                <img
-                    className="object-contain max-h-36"
-                    key={index}
-                    src={planos.src}
-                />
-            </div>
-            {showSaberMais &&
-                <div className={
-                    'flex justify-center items-center text-2xl text-base-base bg-base-tertiary ease-in-out duration-900 -translate-y-5' +
-                    (showSaberMais && ' translate-y-0 scale-110')
-                }
-                >
-                    <h4>Saber mais</h4>
-                </div>
-            }
+  const [saberMais, setSaberMais] = useState(animateSaberMaisHide);
 
+  return (
+    <motion.div
+      whileHover={{
+        scale: 1.1,
+        transition: { duration: 0.5 },
+      }}
+    >
+      <div className="flex flex-col">
+        <div
+          className="flex flex-col justify-center items-center shadow-lg p-6 h-40
+             rounded z-50"
+          onMouseOver={() => setSaberMais(animateSaberMaisShow)}
+          onMouseOut={() => setSaberMais(animateSaberMaisHide)}
+        >
+          <img
+            className="object-contain max-h-36"
+            key={index}
+            src={planos.src}
+          />
         </div>
-    )
-}
+        <motion.div
+          className="flex justify-center items-center text-2xl text-base-base bg-base-tertiary z-0"
+          animate={{ y: saberMais }}
+          transition={{ duration: 0.5 }}
+        >
+          <div>
+            <h4>Saber mais</h4>
+          </div>
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+};
 
 export default CardPlanoSaude;
