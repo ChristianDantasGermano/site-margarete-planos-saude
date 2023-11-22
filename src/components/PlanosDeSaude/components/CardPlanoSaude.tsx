6b28acb4bd78a57809fc8a0d5b55258c0a994db5
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2023-11-22 14:05:09 +0000

    melhorias no modal

--- src/components/PlanosDeSaude/components/CardPlanoSaude.tsx
+++ src/components/PlanosDeSaude/components/CardPlanoSaude.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { planoLogos } from "..";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Button, Modal } from "flowbite-react";
 
 type CardPlanoSaude = {
@@ -22,17 +22,18 @@ const CardPlanoSaude: React.FC<CardPlanoSaude> = ({ index, planos }) => {
           scale: 1.1,
           transition: { duration: 0.5 },
         }}
+        whileTap={{ scale: 0.9 }}
       >
         <div className="flex flex-col">
           <div
-            className="flex flex-col bg-base-base justify-center items-center shadow-lg p-6 h-40
+            className="flex flex-col bg-base-base justify-center items-center shadow-lg p-8 h-40
              rounded z-10 cursor-pointer"
             onMouseOver={() => setSaberMais(animateSaberMaisShow)}
             onMouseOut={() => setSaberMais(animateSaberMaisHide)}
             onClick={() => setOpenModal(true)}
           >
             <img
-              className="object-contain max-h-36"
+              className="object-contain max-h-32 max-w-xs"
               key={index}
               src={planos.src}
             />
@@ -48,33 +49,49 @@ const CardPlanoSaude: React.FC<CardPlanoSaude> = ({ index, planos }) => {
           </motion.div>
         </div>
       </motion.div>
-      <Modal
-        show={openModal}
-        size="4xl"
-        onClose={() => setOpenModal(false)}
-        popup
-      >
-        <Modal.Header />
-        <Modal.Body>
-          <div className="flex flex-col justify-center items-center">
-            <img
-              className="object-contain max-h-24"
-              key={index}
-              src={planos.src}
-            />
-            <div className="p-2 m-2">
-              {planos.paragrafos?.map((value) => (
-                <p className="font-belleza text-left p-1">{value}</p>
-              ))}
-              {planos.obs && (
-                <p className="font-belleza text-left font-semibold p-1">
-                  {planos.obs}
-                </p>
-              )}
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
+      <AnimatePresence>
+        {openModal && (
+          <motion.div
+            key="modal"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 0 }}
+            exit={{ opacity: 0 }}
+          >
+            <Modal
+              show={openModal}
+              size="4xl"
+              onClose={() => setOpenModal(false)}
+              popup
+            >
+              <Modal.Header />
+              <Modal.Body>
+                <div className="flex flex-col justify-center items-center">
+                  <img
+                    className="object-contain max-h-28 max-w-xs"
+                    key={index}
+                    src={planos.src}
+                  />
+                  <div className="p-2 m-2">
+                    {planos.paragrafos?.map((value) => (
+                      <p className="indent-8 font-belleza text-left p-1 text-lg">
+                        {value}
+                      </p>
+                    ))}
+                    {planos.obs && (
+                      <p className="font-belleza text-left font-semibold p-1 text-lg">
+                        {planos.obs}
+                      </p>
+                    )}
+                  </div>
+                  <button className="bg-base-tertiary font-belleza font-semibold text-2xl p-2 w-1/3 shadow-lg hover:shadow-2xl">
+                    Saber mais
+                  </button>
+                </div>
+              </Modal.Body>
+            </Modal>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
