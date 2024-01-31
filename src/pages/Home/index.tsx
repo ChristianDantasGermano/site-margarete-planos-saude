10970e7e86be69e34a27366bd57e07b555bdc8ff
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2024-01-31 15:42:23 +0000

    Adicionado uma nova imagem a home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,13 +3,16 @@ import PlanoSaudeOptions from "./components/PlanosDeSaude";
 import SeguradoraOptions from "./components/Seguradoras";
 import Apresentacao from "./components/Apresentacao";
 import Caracteristicas from "./components/Caracteristicas";
+import Imagem from "./components/Imagem";
+import Carroceu from "./components/Carroceu_2";
 
 function Home() {
   return (
     <div className="z-0 bg-base-base flex-grow ">
-      <Slider />
+      <Carroceu />
       <Apresentacao />
       <Caracteristicas />
+      <Imagem />
       <PlanoSaudeOptions />
       <SeguradoraOptions />
       <div id="modal-portal"></div>
