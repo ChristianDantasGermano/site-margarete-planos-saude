d36252029fd8f2582bf235ddd966db65acc41b22
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2024-02-08 15:08:24 +0000

    Melhoria no footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,28 +1,25 @@
-import { Link } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
-import { rotas } from "../Navbar";
+import { RiWhatsappFill } from "react-icons/ri";
 export default function Footer() {
   return (
-    <footer className="flex flex-col justify-center bg-base-primary items-center p-16">
-      <div className="grid grid-cols-3 gap-6 w-8/12">
-        <div className="col-span-1 flex justify-center items-center">
+    <footer className="flex flex-col justify-start bg-base-primary  p-7">
+      <div className="flex justify-center gap-96 pt-8 px-12">
+        <div className="flex justify-center items-center">
           <img src={Logo} />
         </div>
-        <div className="col-span-2 flex flex-col">
-          <ul className="flex-none flex-col lg:flex items-center gap-6 ">
-            {rotas.map((rota, index) => (
-              <Link key={index} to={rota.to}>
-                <li
-                  className="transition ease-in-out delay-50 
-                              hover:scale-110 duration-100 font-belleza
-                               text-2xl uppercase active:text-neutral-600"
-                >
-                  {rota.label}
-                </li>
-              </Link>
-            ))}
-          </ul>
+        <div className="flex flex-col justify-center items-center">
+          <h4 className="font-belleza font-normal text-xl">
+            Tem dúvidas? Tire todas suas dúvidas pelo WhatsApp
+          </h4>
+          <div className="p-2 cursor-pointer">
+            <RiWhatsappFill size={60} />
+          </div>
         </div>
+      </div>
+      <div className="flex justify-center items-center mt-6">
+        <h6 className="font-belleza font-normal text-sm">
+          Christian Dantas Germano @ 2024
+        </h6>
       </div>
     </footer>
   );
