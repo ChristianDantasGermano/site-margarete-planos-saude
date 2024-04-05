3f43bb434a5ebcdf48a265859c086eb0054a7bb1
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2024-04-05 18:23:18 +0000

    correção na resolução da imagem

--- src/pages/Blog/components/Introducao/index.tsx
+++ src/pages/Blog/components/Introducao/index.tsx
@@ -2,17 +2,17 @@ import { AsyncImage } from "loadable-image";
 import { Blur } from "transitions-kit";
 export default function Introducao() {
   return (
-    <div className="flex flex-col md:flex-row w-full">
-      <div className="flex grow-0 flex-col md:max-w-xl 2xl:max-w-xl">
+    <div className="grid grid-cols-0 md:grid-cols-2  w-full">
+      <div className="flex grow-0 flex-col">
         <h1 className="font-dancingScript font-bold text-4xl md:text-6xl text-gray-700 md:pt-4 bg-base">
           Margarete planos de saúde
         </h1>
-        <h4 className="font-sans text-sm md:text-base text-gray-700 mt-2 text-justify">
-          Especialista em consultoria e corretagem de planos de saúde
-          individuais, familiares, por adesão e para micro, pequenas e médias
-          empresas.
-        </h4>
-        <h4 className="font-sans text-sm md:text-base text-gray-700 text-justify">
+        <p className="font-sans text-sm md:text-base text-gray-700 mt-2 text-justify">
+          Especialista em consultoria e corretagem de planos de
+          saúdeindividuais, familiares, por adesão e para micro, pequenas e
+          médias empresas.
+        </p>
+        <p className="font-sans text-sm md:text-base text-gray-700 text-justify">
           No mundo complexo dos planos de saúde, é comum sentir-se perdido e
           confuso ao navegar pelas diversas opções disponíveis. É nesse momento
           que os corretores de planos de saúde entram em cena. Nossa empresa se
@@ -21,11 +21,11 @@ export default function Introducao() {
           explorar os benefícios e serviços que nossos corretores podem
           oferecer, proporcionando uma experiência tranquila e personalizada
           para você e sua família.
-        </h4>
+        </p>
       </div>
-      <div className="overflow-auto w-full h-full mt-4 md:m-4 flex grow justify-center items-center">
+      <div className="flex justify-center text-center overflow-auto w-full h-full mt-4 md:m-4 items-center p-4">
         <AsyncImage
-          className="object-top md:max-h-[480px] max-h-[360px] max-w-xs md:max-w-md h-screen w-full rounded drop-shadow-lg"
+          className="object-top md:max-h-[460px] 2xl:max-h[560px] max-h-[360px] max-w-md h-screen w-full rounded drop-shadow-lg"
           src="../Blog/photos/ma_perfil04.png"
           Transition={(props) => <Blur radius={10} {...props} />}
           loader={<div className="bg-base-tertiary" />}
